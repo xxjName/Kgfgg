6d5667b391b26f7d9746492f04fae359adfba667
xxjName <xiexiaojun1995@163.com>
2021-03-09 20:05:36 +0800

    移除独立算法到工具类中

--- src/main/java/com/am/WritingAm.java
+++ src/main/java/com/am/WritingAm.java
@@ -1,5 +1,7 @@
 package com.am;
 
+import com.utils.LettersUtils;
+
 import java.util.*;
 
 /***
@@ -43,7 +45,7 @@ public class WritingAm {
         }
         arrNumber.append("}");
         //Recursive implementation of multi array arrangement
-        List<String[]> resultList= integrationLetters(dataList,0,null);
+        List<String[]> resultList= LettersUtils.integrationLetters(dataList,0,null);
 
         System.out.println(arrNumber.toString());
         System.out.print("Output:");
@@ -55,32 +57,6 @@ public class WritingAm {
                 System.out.print(s);
             }
         }
-    }
-    private static List<String[]> integrationLetters(List<String[]> dataList, int index, List<String[]> resultList){
-        //Determine whether the collection is empty
-        if(index==dataList.size()){
-            return resultList;
-        }
-        List<String[]> resultStr=new ArrayList<String[]>();
-        if(index==0){//The first column array will add as many permutation data as there are letters by default
-            String[] dataArr=dataList.get(0);
-            for(String s : dataArr){
-                resultStr.add(new String[]{s});
-            }
-        }else{
-            String[] dataOb=dataList.get(index);
-            for(String[] dataStr: resultList){
-                for(String str : dataOb){
-                    //Copy array and expand new elements
-                    String[] dataArrCopy=new String[dataStr.length+1];
-                    System.arraycopy(dataStr, 0, dataArrCopy, 0, dataStr.length);
-                    dataArrCopy[dataArrCopy.length-1]=str;
-                    //Append to result set
-                    resultStr.add(dataArrCopy);
-                }
-            }
-        }
-        return integrationLetters(dataList,++index,resultStr);
     }
 
 }
